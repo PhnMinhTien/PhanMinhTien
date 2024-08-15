4c762ef34ab9d7dbb0d311065db3607225911196
PhnMinhTien <mtphan655@gmail.com>
2024-08-15 20:13:38 +0700

    Add files via upload

---
+++ buoi1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 18:45:03 2024
+
+@author: mtpha
+"""
+
+print("hello world")
---
+++ buoi1a.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 18:45:03 2024
+
+@author: PhanMinhTien
+"""
+
+print("hello world")
+print("I like 6.100A!")
